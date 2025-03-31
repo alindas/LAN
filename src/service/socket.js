bf2604c027d965a89bf1d0bbd25d48eb1f1f6ed4
lihouhua <hhrhee@163.com>
2025-03-31 14:45:19 +0800

    feat: 单向自动互联&连接详情

--- src/service/socket.js
+++ src/service/socket.js
@@ -1,27 +1,32 @@
 import net from 'net'
 import readline from 'readline'
+import dayjs from 'dayjs';
+import ora from 'ora';
 
+const spinner = ora('waiting...')
 
 export default function (cfg, logger) {
   const PORT = cfg.port ?? 3000;
   // 创建 TCP 服务器
   const server = net.createServer((socket) => {
-    console.log('客户端已连接');
+    const clientHost = socket.remoteAddress
+    console.log('客户端已连接', clientHost);
+    handleConnect(clientHost);
 
     // 监听客户端发送的数据
     socket.on('data', (data) => {
-      console.log(`收到消息: ${data}`);
+      console.log(`收到 ${clientHost} 消息: ${data}`);
       logger.info(data.toString())
     });
 
     // 监听客户端断开连接
     socket.on('end', () => {
-      console.log('客户端已断开连接');
+      console.log('客户端已断开连接', clientHost);
     });
   });
 
   const HOST = '0.0.0.0'; // 监听所有网络接口
-  server.listen(PORT, HOST, () => {
+  server.listen(PORT, () => {
     console.log(`服务器正在监听 ${HOST}:${PORT}`);
   });
 
@@ -29,18 +34,19 @@ export default function (cfg, logger) {
   let client;
 
   function handleConnect(host) {
-
+    if (client && client.remoteAddress == host) return
     let _client = new net.Socket()
+    spinner.start()
     _client.connect(PORT, host, () => {
       if (client) {
         client.end()
       }
       client = _client
-      console.log(`已连接到 ${host}:${PORT}`);
+      spinner.succeed(`已连接到 ${host}:${PORT}`);
     });
 
     _client.on('error', () => {
-      console.log(`${host}:${REMOTE_PORT}连接失败`);
+      spinner.fail(`${host}:${PORT} 连接失败`)
     })
 
   }
@@ -49,6 +55,7 @@ export default function (cfg, logger) {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
+    terminal: false
   });
 
   rl.on('line', (input) => {
@@ -57,6 +64,7 @@ export default function (cfg, logger) {
     } else if (client) {
       // 将输入的内容发送到另一台电脑
       client.write(input);
+      console.log(`---------- ${dayjs().format('HH:mm:ss')}`)
     }
   });
 
